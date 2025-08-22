3ad741740fcab9add9fa877cbabd70e75c06ebd2
Marcus Miss <marcus.miss@mac.com>
2025-08-22 10:14:14 +0200

    Try another way of redirect

--- build_docc.sh
+++ build_docc.sh
@@ -10,8 +10,4 @@ xcrun docc process-archive transform-for-static-hosting \
     --output-path "docc" \
     --hosting-base-path "https://marcusmiss.github.io/mmecletic"
 
-echo '<html><body><script>window.location.href += "/documentation/mmecletic"</script></body></html>' > docc/index.html
-
-# To test locally:
-# set: --hosting-base-path ""
-# run: python3 -m http.server -d docc
+echo '<!DOCTYPE html><html><head><meta http-equiv = "refresh" content = "0; url = https://marcusmiss.github.io/MMEcletic/documentation/mmecletic/" /></head><body></body></html>' > docc/index.html
