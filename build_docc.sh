8c40e5677c2c39229b94f67b30acde2e756e11e9
Marcus Miss <marcus.miss@mac.com>
2025-08-22 09:15:32 +0200

    Created manifest .spi.yml for docc for SPI

--- build_docc.sh
+++ build_docc.sh
@@ -8,6 +8,10 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/MMEcletic.doccarchive" \
     --output-path "docc" \
-    --hosting-base-path "https://marcusmiss.github.io/mmecletic"
+    --hosting-base-path "" # "https://marcusmiss.github.io/mmecletic"
 
-echo '<script>window.location.href += "/documentation/mmecletic"</script>' > docc/index.html
+echo '<html><body><script>window.location.href += "/documentation/mmecletic"</script></body></html>' > docc/index.html
+
+# To test locally:
+# set: --hosting-base-path ""
+# run: python3 -m http.server -d docc
