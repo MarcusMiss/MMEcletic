e08c47f02b66f0b47389c7954553fdd26cc5fca6
Marcus Miss <marcus.miss@mac.com>
2025-08-22 08:20:07 +0200

    Adjusted --hosting-base-path

--- build_docc.sh
+++ build_docc.sh
@@ -8,6 +8,6 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/MMEcletic.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "https://github.com/MarcusMiss/MMEcletic"
+    --hosting-base-path "https://marcusmiss.github.io"
 
 echo '<script>window.location.href += "/documentation/mmecletic"</script>' > .docs/index.html
